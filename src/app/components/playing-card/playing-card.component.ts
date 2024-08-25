c8c6d9d1dd75413f02e04229f1fb314e5477fd83
LuLuAyA <ludovichaase67160@gmail.com>
2024-08-25 15:24:52 +0200

    [import class monster]

--- src/app/components/playing-card/playing-card.component.ts
+++ src/app/components/playing-card/playing-card.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Monster } from '../../models/monster.model';
 
 @Component({
   selector: 'app-playing-card',
   standalone: true,
   imports: [],
   templateUrl: './playing-card.component.html',
-  styleUrl: './playing-card.component.css'
+  styleUrl: './playing-card.component.css',
 })
 export class PlayingCardComponent {
-
+  @Input({
+    alias: 'my-monster',
+    transform: (value: Monster) => {
+      value.hp = value.hp / 2;
+      return value;
+    },
+  })
+  monster: Monster = new Monster();
 }
---
+++ src/app/models/monster.model.ts
@@ -0,0 +1,9 @@
+export class Monster {
+  name: string = 'My Monster';
+  hp: number = 40;
+  figureCaption: string = 'N°001 Monster';
+  attackName: string = 'Vive attaque';
+  attackStrength: number = +10;
+  attackDescription: string =
+    "Lancer un pièce si c'est face, cette attaque fait 10 dégats de plus";
+}
