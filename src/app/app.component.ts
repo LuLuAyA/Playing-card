0e5ff847005aba779ac3aaf2e87a6233ae778296
LuLuAyA <ludovichaase67160@gmail.com>
2024-08-25 17:32:14 +0200

    [signal model search-bar]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,28 @@
 import { Component } from '@angular/core';
 import { PlayingCardComponent } from './components/playing-card/playing-card.component';
 import { Monster } from './models/monster.model';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PlayingCardComponent],
+  imports: [PlayingCardComponent, SearchBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   monster1!: Monster;
+  count: number = 0;
+  search = '';
 
   constructor() {
     this.monster1 = new Monster();
     this.monster1.name = 'Pikachu';
     this.monster1.hp = 40;
     this.monster1.figureCaption = 'N°002 Pikachu';
+  }
+
+  increaseCount() {
+    this.count++;
   }
 }
---
+++ src/app/search-bar/search-bar.component.ts
@@ -0,0 +1,27 @@
+import {
+  Component,
+  EventEmitter,
+  input,
+  Input,
+  model,
+  output,
+  Output,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-search-bar',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './search-bar.component.html',
+  styleUrl: './search-bar.component.css',
+})
+export class SearchBarComponent {
+  search = model<string>('initial');
+
+  searchButtonClicked = output({ alias: 'submit' });
+
+  searchClick() {
+    this.searchButtonClicked.emit();
+  }
+}
