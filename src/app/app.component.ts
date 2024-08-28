9e41a737842bbc2ed648a6a6de51d5c5b89d7f02
LuLuAyA <ludovichaase67160@gmail.com>
2024-08-28 17:50:14 +0200

    [boucle]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,30 @@
-import { Component, computed, effect, signal } from '@angular/core';
+import { Component, computed, effect, model, signal } from '@angular/core';
 import { PlayingCardComponent } from './components/playing-card/playing-card.component';
 import { Monster } from './models/monster.model';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { MonsterType } from './utils/monster.utils';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PlayingCardComponent, SearchBarComponent],
+  imports: [PlayingCardComponent, SearchBarComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   monsters!: Monster[];
-  count: number = 0;
-  search = '';
+  search = model('');
 
-  selectedMonsterIndex = signal(1);
-
-  selectedMonster = computed(() => {
-    return this.monsters[this.selectedMonsterIndex()];
+  filteredMonsters = computed(() => {
+    return this.monsters.filter((monster) =>
+      monster.name.includes(this.search())
+    );
   });
 
   constructor() {
-    effect(() => {
-      console.log(this.selectedMonster());
-    });
-
     this.monsters = [];
+
     const monster1 = new Monster();
     monster1.name = 'Pikachu';
     monster1.hp = 40;
@@ -41,11 +38,21 @@ export class AppComponent {
     monster2.hp = 60;
     monster2.figureCaption = 'N°002 Carapuce';
     this.monsters.push(monster2);
-  }
 
-  toggleMonster() {
-    this.selectedMonsterIndex.set(
-      (this.selectedMonsterIndex() + 1) % this.monsters.length
-    );
+    const monster3 = new Monster();
+    monster3.name = 'Bulbizarre';
+    monster3.image = 'assets/img/bulbizarre.png';
+    monster3.type = MonsterType.PLANT;
+    monster3.hp = 60;
+    monster3.figureCaption = 'N°003 Bulbizarre';
+    this.monsters.push(monster3);
+
+    const monster4 = new Monster();
+    monster4.name = 'Salameche';
+    monster4.image = 'assets/img/salameche.png';
+    monster4.type = MonsterType.FIRE;
+    monster4.hp = 60;
+    monster4.figureCaption = 'N°004 Salameche';
+    this.monsters.push(monster4);
   }
 }
